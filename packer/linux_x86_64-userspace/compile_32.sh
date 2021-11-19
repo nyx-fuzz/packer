3fab4507fec608f731a060bdd10ae6e1713622d1
Sergej Schumilo <sergej@schumilo.de>
2021-11-19 15:38:43 +0100

    fix support for 32-bit targets

--- packer/linux_x86_64-userspace/compile_32.sh
+++ packer/linux_x86_64-userspace/compile_32.sh
@@ -10,8 +10,8 @@ fi
 if [ "$LEGACY_MODE" = "ON" ]
 then
   # old kAFL mode shared library
-  gcc -shared -O0 -m32 -Werror -DLEGACY_MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c  src/netfuzz/syscalls.c -I../../agents -o bin32/ld_preload_fuzz_legacy.so -ldl -Isrc
-  gcc -shared -O0 -m32 -Werror -DLEGACY_MODE -DNO_PT_NYX -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c  src/netfuzz/syscalls.c -I../../agents -o bin32/ld_preload_fuzz_legacy_no_pt.so -ldl -Isrc
+  gcc -shared -O0 -m32 -Werror -DLEGACY_MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c  src/netfuzz/syscalls.c -I../../ -o bin32/ld_preload_fuzz_legacy.so -ldl -Isrc
+  gcc -shared -O0 -m32 -Werror -DLEGACY_MODE -DNO_PT_NYX -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c  src/netfuzz/syscalls.c -I../../ -o bin32/ld_preload_fuzz_legacy_no_pt.so -ldl -Isrc
 else
   # latest and greatest nyx shared library
 
@@ -19,34 +19,34 @@ else
   then
     MODE="${UDP_MODE} ${CLIENT_MODE} ${DEBUG_MODE} ${STDOUT_STDERR_DEBUG}"
     echo "MODES => $MODE"
-    clang -shared -g -O0 -m32 -Werror $EXTRA $MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c src/netfuzz/inject.c src/netfuzz/syscalls.c src/netfuzz/socket_cache.c -I../../agents -DNET_FUZZ -I$NYX_SPEC_FOLDER -o bin64/ld_preload_fuzz.so -ldl -Isrc
-    clang -shared -g -O0 -m32 -Werror -DNO_PT_NYX $EXTRA $MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c src/netfuzz/inject.c src/netfuzz/syscalls.c src/netfuzz/socket_cache.c -I../../agents -DNET_FUZZ -I$NYX_SPEC_FOLDER -o bin64/ld_preload_fuzz_no_pt.so -ldl -Isrc
+    clang -shared -g -O0 -m32 -Werror $EXTRA $MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c src/netfuzz/inject.c src/netfuzz/syscalls.c src/netfuzz/socket_cache.c -I../../ -DNET_FUZZ -I$NYX_SPEC_FOLDER -o bin64/ld_preload_fuzz.so -ldl -Isrc
+    clang -shared -g -O0 -m32 -Werror -DNO_PT_NYX $EXTRA $MODE -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/misc/harness_state.c src/netfuzz/inject.c src/netfuzz/syscalls.c src/netfuzz/socket_cache.c -I../../ -DNET_FUZZ -I$NYX_SPEC_FOLDER -o bin64/ld_preload_fuzz_no_pt.so -ldl -Isrc
 
   else
 
   if [ -n "$NYX_SPEC_FOLDER" ]
     then
-      gcc -shared -O0 -m32 -Werror -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/netfuzz/syscalls.c src/misc/harness_state.c -I../../agents  -I$NYX_SPEC_FOLDER -o bin32/ld_preload_fuzz.so -ldl -Isrc
-      gcc -shared -O0 -m32 -Werror -DNO_PT_NYX -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/netfuzz/syscalls.c src/misc/harness_state.c -I../../agents  -I$NYX_SPEC_FOLDER -o bin32/ld_preload_fuzz_no_pt.so -ldl -Isrc
+      gcc -shared -O0 -m32 -Werror -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/netfuzz/syscalls.c src/misc/harness_state.c -I../../  -I$NYX_SPEC_FOLDER -o bin32/ld_preload_fuzz.so -ldl -Isrc
+      gcc -shared -O0 -m32 -Werror -DNO_PT_NYX -fPIC src/ld_preload_fuzz.c src/misc/crash_handler.c src/netfuzz/syscalls.c src/misc/harness_state.c -I../../  -I$NYX_SPEC_FOLDER -o bin32/ld_preload_fuzz_no_pt.so -ldl -Isrc
     fi
   fi
 fi
 
 gcc -m32  src/libnyx.c -o bin32/libnyx.so -shared -fPIC -Wall -std=gnu11 -Wl,-soname,libnyx.so
 
-# pt mode builds 
-gcc -O0 -m32 -Werror src/htools/habort.c -I../../agents -o bin32/habort
-gcc -O0 -m32 -Werror src/htools/hcat.c -I../../agents -o bin32/hcat
-gcc -O0 -m32 -Werror src/htools/hget.c -I../../agents -o bin32/hget
+# pt mode builds
+gcc -O0 -m32 -Werror src/htools/habort.c -I../../ -o bin32/habort
+gcc -O0 -m32 -Werror src/htools/hcat.c -I../../ -o bin32/hcat
+gcc -O0 -m32 -Werror src/htools/hget.c -I../../ -o bin32/hget
 #gcc -O0 -m32 -Werror src/htools/hget_bulk.c -I../../agents -o bin32/hget_bulk
-gcc -O0 -m32 -Werror src/htools/hpush.c -I../../agents -o bin32/hpush
+gcc -O0 -m32 -Werror src/htools/hpush.c -I../../ -o bin32/hpush
 
 # no-pt mode builds
-gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/habort.c -I../../agents -o bin32/habort_no_pt
-gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hcat.c -I../../agents -o bin32/hcat_no_pt
-gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hget.c -I../../agents -o bin32/hget_no_pt
+gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/habort.c -I../../ -o bin32/habort_no_pt
+gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hcat.c -I../../ -o bin32/hcat_no_pt
+gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hget.c -I../../ -o bin32/hget_no_pt
 #gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hget_bulk.c -I../../agents -o bin32/hget_bulk_no_pt
-gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hpush.c -I../../agents -o bin32/hpush_no_pt
+gcc -O0 -m32 -Werror -DNO_PT_NYX src/htools/hpush.c -I../../ -o bin32/hpush_no_pt
 
 # loader support both modes (PT & NO-PT)
 #gcc -O0 -m32 -static -Werror src/loader.c -I../../agents -o bin32/loader
