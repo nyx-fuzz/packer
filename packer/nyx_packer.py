f201fa25d3cc0698d205c6bc14ab16aa7e95519b
Sergej Schumilo <sergej@schumilo.de>
2022-04-20 14:11:27 +0200

    set upper limit of AFL bitmap (AFL_MAP_SIZE)

--- packer/nyx_packer.py
+++ packer/nyx_packer.py
@@ -372,7 +372,7 @@ def compile(config):
     if DELAYED_INIT:
         download_script += "DELAYED_NYX_FUZZER_INIT=ON "
     if COVERAGE_MODE:
-        download_script += "NYX_AFL_PLUS_PLUS_MODE=ON "
+        download_script += "NYX_AFL_PLUS_PLUS_MODE=ON AFL_MAP_SIZE=8388608 "
     if FAST_EXIT_MODE:
         download_script += "NYX_FAST_EXIT_MODE=TRUE "
     if NET_FUZZ_MODE:
